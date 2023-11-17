800d5db063226ac8bb3fa4887f1bfc7a9afd560a
SIRIUS KIM <jmoumnet@gmail.com>
2023-11-17 17:33:35 +0900

    231117 Toy Project - MusicLife
    1. Custom cell 초기값 세팅
    2. Cell size 계산

--- MusicLife/HomeViewController.swift
+++ MusicLife/HomeViewController.swift
@@ -7,10 +7,40 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrackCollectionViewCell", for: indexPath) as? TrackCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    // set CollectionView Header/Footer
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            return UICollectionReusableView()
+        default:
+            return UICollectionReusableView()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 20
+        let margin: CGFloat = 20
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing - margin * 2) / 2
+        let height: CGFloat = width + 60
+        return CGSize(width: width, height: height)
+    }
 }
---
+++ MusicLife/TrackCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TrackCollectionViewCell.swift
+//  MusicLife
+//
+//  Created by JONG SOO KIM on 11/17/23.
+//
+
+import UIKit
+
+class TrackCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var trackThumbnail: UIImageView!
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var trackArtist: UILabel!
+    
+}
